62a0104bd528088544b465ee2df8ae280b240271
Matthew Kim <matthew.kim@petcircle.com.au>
2018-10-18 09:23:12 +1100

    modified

--- src/au/com/petcircle/JenkinsPP/global/serviceChecker.groovy
+++ src/au/com/petcircle/JenkinsPP/global/serviceChecker.groovy
@@ -3,7 +3,7 @@
 package au.com.petcircle.JenkinsPP.global
 
 class serviceChecker {
-    au.com.petcircle.JenkinsPP.global.serviceValues v = new au.com.petcircle.JenkinsPP.global.serviceValues();
+    serviceValues v = new serviceValues();
 
     serviceValues getValues(String serviceName){
         switch (serviceName){
@@ -88,4 +88,43 @@ class serviceChecker {
         return v.getBaseIp();
     }
 
+}
+
+class serviceValues {
+    int min;
+    int max;
+    String baseName;
+    String baseIp;
+
+    int getMin() {
+        return min
+    }
+
+    void setMin(int min) {
+        this.min = min
+    }
+
+    int getMax() {
+        return max
+    }
+
+    void setMax(int max) {
+        this.max = max
+    }
+
+    String getBaseName() {
+        return baseName
+    }
+
+    void setBaseName(String baseName) {
+        this.baseName = baseName
+    }
+
+    String getBaseIp() {
+        return baseIp
+    }
+
+    void setBaseIp(String baseIp) {
+        this.baseIp = baseIp
+    }
 }

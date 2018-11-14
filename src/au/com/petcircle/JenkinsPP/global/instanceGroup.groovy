d49573a061197d985e08d6a9d982c856916b70f7
Matthew Kim <matthew.kim@petcircle.com.au>
2018-11-14 13:35:31 +1100

    modified

--- src/au/com/petcircle/JenkinsPP/global/instanceGroup.groovy
+++ src/au/com/petcircle/JenkinsPP/global/instanceGroup.groovy
@@ -4,52 +4,68 @@ package au.com.petcircle.JenkinsPP.global
 
 class instanceGroup {
 
-    String devIP1, devIP2, prodIP1, prodIP2, devName1, devName2, prodName1, prodName2
+    String devIP1, devIP2, devIP3, prodIP1, prodIP2, prodIP3, devName1, devName2, devName3, prodName1, prodName2, prodName3
     void setSvrIPs(String service) {
 
         switch (service) {
             case "product" : case "review":
                 devIP1 = "10.40.10.21"
                 devIP2 = "10.40.10.22"
+                devIP3 = "10.40.10.26"
                 prodIP1 = "10.50.10.21"
                 prodIP2 = "10.50.10.22"
+                prodIP3 = "10.50.10.26"
                 devName1 = "yellow01-dev"
                 devName2 = "yellow02-dev"
+                devName3 = "yellow11-dev"
                 prodName1 = "yellow01"
                 prodName2 = "yellow02"
+                prodName3 = "yellow11"
                 break
 
             case "merch": case "order": case "search":
                 devIP1 = "10.40.10.31"
                 devIP2 = "10.40.10.32"
+                devIP3 = "10.40.10.36"
                 prodIP1 = "10.50.10.31"
                 prodIP2 = "10.50.10.32"
+                prodIP3 = "10.50.10.36"
                 devName1 = "black01-dev"
                 devName2 = "black02-dev"
+                devName3 = "black11-dev"
                 prodName1 = "black01"
                 prodName2 = "black02"
+                prodName3 = "black11"
                 break
 
             case "payment" : case "customer": case "auth":
                 devIP1 = "10.40.10.41"
                 devIP2 = "10.40.10.42"
+                devIP3 = "10.40.10.46"
                 prodIP1 = "10.50.10.41"
                 prodIP2 = "10.50.10.42"
+                prodIP3 = "10.50.10.46"
                 devName1 = "red01-dev"
                 devName2 = "red02-dev"
+                devName3 = "red11-dev"
                 prodName1 = "red01"
                 prodName2 = "red02"
+                prodName3 = "red11"
                 break
 
             case "shipping":
                 devIP1 = "10.40.10.51"
                 devIP2 = "10.40.10.52"
+                devIP3 = "10.40.10.56"
                 prodIP1 = "10.50.10.51"
                 prodIP2 = "10.50.10.52"
+                prodIP3 = "10.50.10.56"
                 devName1 = "chocolate01-dev"
                 devName2 = "chocolate02-dev"
+                devName3 = "chocolate11-dev"
                 prodName1 = "chocolate01"
                 prodName2 = "chocolate02"
+                prodName3 = "chocolate11"
                 break
 
             case "shiba":
@@ -89,6 +105,16 @@ class instanceGroup {
                 return devIP2
             }
 
+        }else if(whereto == "blue")
+        {
+            if (isProd)
+            {
+                return prodIP2
+            }else
+            {
+                return devIP3
+            }
+
         }
     }
 
@@ -116,6 +142,15 @@ class instanceGroup {
                 {
                     return devName2
                 }
+            }else if (whereto == "blue")
+            {
+                if (isProd)
+                {
+                    return prodName2
+                }else
+                {
+                    return devName3
+                }
             }
 
         }else{
@@ -137,8 +172,16 @@ class instanceGroup {
                 {
                     return "labrador-"+devName2
                 }
+            }else if (whereto == "blue")
+            {
+                if (isProd)
+                {
+                    return "labrador-"+prodName2
+                }else
+                {
+                    return "labrador-"+devName3
+                }
             }
         }
     }
-
 }
